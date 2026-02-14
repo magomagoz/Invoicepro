954763c96c4697f0b4f213d70553569dfc3c542e
magomagoz <magostinienrico@gmail.com>
2026-02-14 16:37:58 +0100

    Update invoicepro.py

--- invoicepro.py
+++ invoicepro.py
@@ -267,7 +267,7 @@ elif st.session_state.pagina == "form":
     col1, col2 = st.columns(2, gap="medium")
     
     with col1:
-        data = st.date_input("**📅 Data**", value=datetime.now())
+        data = st.date_input("**📅 Data**", value=datetime.now().isoformat())
         anno_selezionato = st.session_state.anno_selezionato
         numero = st.text_input("**🔢 Numero Protocollo**", 
                               value=f"{anno_selezionato}/{len(st.session_state.dati_fatture[tipo])+1}")
